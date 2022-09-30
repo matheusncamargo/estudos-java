5aeef3ed9896352d233390e41dae35bfabf4f814
Matheus Nogueira Camargo <109735749+matheusncamargo@users.noreply.github.com>
2022-09-30 20:04:12 -0300

    Update hello.java

--- hello.java
+++ hello.java
@@ -39,6 +39,21 @@ public class Main {
 
 }
 
+/*
+		Scanner input = new Scanner(System.in);
+
+		String s1, s2, s3;
+		System.out.println("Digite 3 dados: ");
+		s1 = input.nextLine();
+		s2 = input.nextLine();
+		s3 = input.nextLine();
+		
+		System.out.println("Dados digitados:");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+*/
+
 /* 
  	Scanner input = new Scanner(System.in);
 		
